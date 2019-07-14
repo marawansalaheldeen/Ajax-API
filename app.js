77307f2220e2814d9a37ddaab94ef844a9f146f4
gs <marawansalahuldeen@gmail.com>
2019-07-14 15:25:59 +0200

    initializing app

---
+++ app.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var app  = express();
+
+app.use(express.static(__dirname));
+var port =3000;
+
+app.listen(port,()=>{ console.log(`server listen on port ${port}`)});
